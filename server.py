94742a281283d4602b6b379545f2601273d8ce9a
Yaoyao <45887805+wyy511511@users.noreply.github.com>
2024-07-11 17:17:47 -0600

    update options to ensure consistency

--- server.py
+++ server.py
@@ -77,6 +77,38 @@ models = {
     },
 }
 
+base_options = {
+
+    "stream":True,
+    "num_keep": 5,
+    "seed": 40,
+    "num_predict": 100, #100
+    "top_k": 20,
+    "top_p": 0.9,
+    "tfs_z": 0.5,
+    "typical_p": 0.7,
+    "repeat_last_n": 33,
+    "temperature": 0,
+    "repeat_penalty": 1.2,
+    "presence_penalty": 1.5,
+    "frequency_penalty": 1.0,
+    "mirostat": 1,
+    "mirostat_tau": 0.8,
+    "mirostat_eta": 0.6,
+    "penalize_newline": True,
+    "stop": ["\n", "user:"],
+    "numa": False,
+    "num_ctx": 1024,
+    "num_batch": 2,
+    "num_gpu": 1,
+    "main_gpu": 0,
+    "low_vram": False,
+    "f16_kv": True,
+    "vocab_only": False,
+    "use_mmap": True,
+    "use_mlock": False,
+    "num_thread": 8
+}
 def pull_model(model_name):
     try:
         ollama.pull(model_name)
@@ -110,6 +142,7 @@ def detect():
                       ],
             stream=True,
             format="json",
+            options=base_options
         )
         print("here")
         # print(response)
