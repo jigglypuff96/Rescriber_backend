2459cae3fedc0bb70b5c606095b8bca410ba0e0a
jigglypuff96 <>
2024-07-11 21:52:51 -0400

    add back option with seed only

--- server.py
+++ server.py
@@ -77,37 +77,39 @@ models = {
     },
 }
 
-base_options = {
+# base_options = {
+#     "num_keep": 5,
+#     "seed": 40,
+#     "num_predict": 100, #100
+#     "top_k": 20,
+#     "top_p": 0.9,
+#     "tfs_z": 0.5,
+#     "typical_p": 0.7,
+#     "repeat_last_n": 33,
+#     "temperature": 0,
+#     "repeat_penalty": 1.2,
+#     "presence_penalty": 1.5,
+#     "frequency_penalty": 1.0,
+#     "mirostat": 1,
+#     "mirostat_tau": 0.8,
+#     "mirostat_eta": 0.6,
+#     "penalize_newline": True,
+#     "stop": ["\n", "user:"],
+#     "numa": False,
+#     "num_ctx": 1024,
+#     "num_batch": 2,
+#     "num_gpu": 1,
+#     "main_gpu": 0,
+#     "low_vram": False,
+#     "f16_kv": True,
+#     "vocab_only": False,
+#     "use_mmap": True,
+#     "use_mlock": False,
+#     "num_thread": 8
+# }
 
-    "stream":True,
-    "num_keep": 5,
-    "seed": 40,
-    "num_predict": 100, #100
-    "top_k": 20,
-    "top_p": 0.9,
-    "tfs_z": 0.5,
-    "typical_p": 0.7,
-    "repeat_last_n": 33,
-    "temperature": 0,
-    "repeat_penalty": 1.2,
-    "presence_penalty": 1.5,
-    "frequency_penalty": 1.0,
-    "mirostat": 1,
-    "mirostat_tau": 0.8,
-    "mirostat_eta": 0.6,
-    "penalize_newline": True,
-    "stop": ["\n", "user:"],
-    "numa": False,
-    "num_ctx": 1024,
-    "num_batch": 2,
-    "num_gpu": 1,
-    "main_gpu": 0,
-    "low_vram": False,
-    "f16_kv": True,
-    "vocab_only": False,
-    "use_mmap": True,
-    "use_mlock": False,
-    "num_thread": 8
+base_options = {
+    "seed": 40
 }
 def pull_model(model_name):
     try:
@@ -141,7 +143,8 @@ def detect():
                       {'role': 'system', 'content': models["detect"]["prompt"]}
                       ],
             stream=True,
-            format="json"
+            format="json",
+             options=base_options
         )
         # options=base_options
         print("detect response ready")
